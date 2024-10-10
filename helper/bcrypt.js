5bf982b2d120b23a7ceebb5fa49d8eebae5ec41f
FildzaHadyan <hadyanf@gmail.com>
2024-10-10 15:00:08 +0700

    bcrypt password helper

--- helper/bcrypt.js
+++ helper/bcrypt.js
@@ -1,3 +1,13 @@
 
 
-var bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
+
+const hashPassword = (password) => {
+  return bcrypt.hashSync(password);
+};
+
+const comparePassword = (password, hash) => {
+  return bcrypt.compareSync(password, hash);
+};
+
+module.exports = { hashPassword, comparePassword };
--- models/user.js
+++ models/user.js
@@ -77,12 +77,12 @@ module.exports = (sequelize, DataTypes) => {
     // }
   });
 
-  User.addHook('beforeCreate', (user) =>{
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(user.password, salt);
+  // User.addHook('beforeCreate', (user) =>{
+  //   const salt = bcrypt.genSaltSync(10);
+  //   const hash = bcrypt.hashSync(user.password, salt);
 
-    user.password = hash
-  })
+  //   user.password = hash
+  // })
 
   return User;
 };
